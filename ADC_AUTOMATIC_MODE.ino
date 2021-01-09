5c1535daa85781854eaa1fefe49e695e3e1b2564
someguy54 <74697453+someguy54@users.noreply.github.com>
2021-01-09 16:12:53 -0300

    Add files via upload
    Automatic ADC Reading using registers

---
+++ ADC_AUTOMATIC_MODE.ino
@@ -0,0 +1,49 @@
+
+const byte adcPin = 0;  // A0
+
+const int MAX_RESULTS = 256;
+
+volatile int results [MAX_RESULTS];
+volatile int resultNumber;
+
+// ADC complete ISR
+ISR (ADC_vect)
+  {
+  if (resultNumber >= MAX_RESULTS)
+    ADCSRA = 0;  // turn off ADC
+  else
+    results [resultNumber++] = ADC;
+  }  // end of ADC_vect
+  
+EMPTY_INTERRUPT (TIMER1_COMPB_vect);
+ 
+void setup ()
+  {
+  Serial.begin (115200);
+  Serial.println ();
+  
+  // reset Timer 1
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+  TCCR1B = bit (CS11) | bit (WGM12);  // CTC, prescaler of 8
+  TIMSK1 = bit (OCIE1B);  // WTF?
+  OCR1A = 39;    
+  OCR1B = 39;   // 20 uS - sampling frequency 50 kHz
+
+  ADCSRA =  bit (ADEN) | bit (ADIE) | bit (ADIF);   // turn ADC on, want interrupt on completion
+  ADCSRA |= bit (ADPS2);  // Prescaler of 16
+  ADMUX = bit (REFS0) | (adcPin & 7);
+  ADCSRB = bit (ADTS0) | bit (ADTS2);  // Timer/Counter1 Compare Match B
+  ADCSRA |= bit (ADATE);   // turn on automatic triggering
+
+  // wait for buffer to fill
+  while (resultNumber < MAX_RESULTS)
+    { }
+    
+  for (int i = 0; i < MAX_RESULTS; i++)
+    Serial.println (results [i]);
+
+  }  // end of setup
+
+void loop () { }
